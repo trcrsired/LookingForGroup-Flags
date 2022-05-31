b3445e34af0201beee7e478b53cf31a7b76b1eb5
trcrsired <uwgghhbcad@gmail.com>
2022-05-30 21:40:30 -0400

    add anzu blocker

---
+++ cn/Core.lua
@@ -0,0 +1,14 @@
+local LFG = LibStub("AceAddon-3.0"):GetAddon("LookingForGroup")
+local languages =
+{
+"安苏",
+}
+
+local flags = {"|cffff0000 (魔兽脑残千千万，安苏玩家占大半) |r"}
+
+local realms =
+{
+["安苏"] = 1,
+}
+
+LFG.flags={languages,flags,realms}
