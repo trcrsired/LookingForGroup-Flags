f4ef14a1c459ce1182433773a364d05e6945fd12
trcrsired <uwgghhbcad@gmail.com>
2022-05-30 21:54:37 -0400

    anzu

--- cn/Core.lua
+++ cn/Core.lua
@@ -1,10 +1,10 @@
 local LFG = LibStub("AceAddon-3.0"):GetAddon("LookingForGroup")
 local languages =
 {
-"安苏",
+"anzu",
 }
 
-local flags = {"|cffff0000 (魔兽脑残千千万，安苏玩家占大半) |r"}
+local flags = {"|cffff0000 !!!! |r"}
 
 local realms =
 {
